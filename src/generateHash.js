7fb12b6d93aa6a9c62f71fc560db3735a16ff12a
Kevin Whitley <krwhitley@gmail.com>
2024-01-19 23:52:46 -0600

    released v2.1.0-next.1 - need tests on generateHash

--- src/generateHash.js
+++ src/generateHash.js
@@ -1,13 +1,13 @@
 import { randomItem } from './randomItem'
 
 // creates a hash of length (length).
-export const generateHash = (length = 6, sets = {}) => {
+export const generateHash = (length = 6, characterSets = {}) => {
   const {
     lower = 'abcdefghijkmnopqrstuvwxyz',
     upper = 'ABCDEFGHJKLMNPQRSTUVWXYZ',
     numeric = '123456789',
     symbols = ''
-  } = options
+  } = characterSets
 
   return Array(length)
           .fill(0)
