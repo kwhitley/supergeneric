ca38e12d9f0ccf9cdff8204f5827805806487025
Kevin Whitley <krwhitley@gmail.com>
2024-01-19 23:47:54 -0600

    released v2.1.0-next.0 - testing new generateHash function

--- src/generateHash.js
+++ src/generateHash.js
@@ -1,16 +1,19 @@
 import { randomItem } from './randomItem'
 
 // creates a hash of length (length).
-export const generateHash = (length = 6) => {
-  const lower = 'abcdefghijkmnopqrstuvwxyz'
-  const upper = 'ABCDEFGHJKLMNPQRTUVWXYZ'
-  const numeric = '2346789'
+export const generateHash = (length = 6, sets = {}) => {
+  const {
+    lower = 'abcdefghijkmnopqrstuvwxyz',
+    upper = 'ABCDEFGHJKLMNPQRSTUVWXYZ',
+    numeric = '123456789',
+    symbols = ''
+  } = options
 
   return Array(length)
           .fill(0)
           .map((v, index) => index
-                            ? randomItem(upper + lower + numeric)
-                            : randomItem(upper + lower) // start with a letter
+                            ? randomItem(upper + (lower || '') + (numeric || '') + (symbols || ''))
+                            : randomItem(upper + (lower || '')) // start with a letter
           )
           .join('')
 }
